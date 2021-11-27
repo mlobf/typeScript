f1537055939ab09cf66a77e65dca13b76051588e
mlobf <marcoslemeborbafilho@gmail.com>
2021-11-27 18:09:06 -0300

    Go

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('oi ksdfa')
+console.log('oi ksdfa');
+
+
+
+const edu = function () {
+    let x = 1;
+    let y = 4;
+
+    for (let x = 0; x < array.length; x++) {
+        const element = array[x];
+    }
+};
