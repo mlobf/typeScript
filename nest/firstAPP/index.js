7401b9afe54f1895c40b015ad9bf2c9a2e0659da
mlobf <marcoslemeborbafilho@gmail.com>
2022-07-05 00:54:27 -0300

    pre catching errors

--- nest/firstAPP/index.js
+++ nest/firstAPP/index.js
@@ -3,5 +3,13 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = "https://jsonplaceholder.typicode.com/todos/1";
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    //console.log(response.data)
+    var todo = response.data;
+    console.log(todo);
+    var ID = todo.id;
+    var title = todo.title;
+    var finished = todo.completed;
+    console.log("This is a beatifull report ".concat(ID));
+    console.log("This is a title report ".concat(title));
+    console.log("This is a finished report ".concat(finished));
 });
--- nest/firstAPP/index.ts
+++ nest/firstAPP/index.ts
@@ -2,7 +2,18 @@ import axios from "axios"
 
 const url = "https://jsonplaceholder.typicode.com/todos/1"
 axios.get(url).then(response => {
-    console.log(response.data)
+    //console.log(response.data)
+    const todo = response.data
+
+    console.log(todo)
+
+    const ID = todo.id
+    const title = todo.title
+    const finished = todo.completed
+
+    console.log(`This is a beatifull report ${ID}`)
+    console.log(`This is a title report ${title}`)
+    console.log(`This is a finished report ${finished}`)
 })
 
 
