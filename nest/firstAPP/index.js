26bd0294fd07bb561d014a321d6314ffd1a4d14a
mlobf <marcoslemeborbafilho@gmail.com>
2022-07-05 00:43:17 -0300

    add ts-node and how to use fetch at node context

--- nest/firstAPP/index.js
+++ nest/firstAPP/index.js
@@ -1,6 +1,7 @@
 "use strict";
 exports.__esModule = true;
-var result = fetch("https://jsonplaceholder.typicode.com/posts/1")
-    .then(function (response) { return response.json(); })
-    .then(function (json) { return console.log(json); });
-console.log(result);
+var axios_1 = require("axios");
+var url = "https://jsonplaceholder.typicode.com/todos/1";
+axios_1["default"].get(url).then(function (response) {
+    console.log(response.data);
+});
--- nest/firstAPP/index.ts
+++ nest/firstAPP/index.ts
@@ -1,10 +1,9 @@
 import axios from "axios"
 
-const url = "https://jsonplaceholder.typicode.com/posts/1"
-const result = async (url) => {
-    axios.get(url)
-}
+const url = "https://jsonplaceholder.typicode.com/todos/1"
+axios.get(url).then(response => {
+    console.log(response.data)
+})
 
-console.log(result)
 
 
