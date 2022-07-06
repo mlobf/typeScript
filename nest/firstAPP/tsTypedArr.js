651135b195229b076a3da79febb2793c499afcd6
mlobf <marcoslemeborbafilho@gmail.com>
2022-07-06 14:44:22 -0300

    Many type in one  typed array

--- nest/firstAPP/tsTypedArr.js
+++ nest/firstAPP/tsTypedArr.js
@@ -3,9 +3,14 @@ var dates = [new Date(), new Date()];
 var carsByMakers = [];
 var car = carsByMakers[0];
 var myCar = carMakers.pop();
-//carMakers.push(100)
+//carMakers.push(100) // vai dar error 
 carMakers.map(function (car) {
     var result = car.toUpperCase();
     console.log(result);
     return result;
 });
+// This arr can receive a Date or string as value, either
+var importantDates = [new Date(), '2022-10-10'];
+importantDates.push('2021-01-01');
+importantDates.push(new Date());
+console.log(importantDates);
--- nest/firstAPP/tsTypedArr.ts
+++ nest/firstAPP/tsTypedArr.ts
@@ -5,7 +5,7 @@ const carsByMakers: string[][] = []
 const car = carsByMakers[0]
 const myCar = carMakers.pop()
 
-//carMakers.push(100)
+//carMakers.push(100) // vai dar error 
 
 carMakers.map((car: string): string => {
     let result: string = car.toUpperCase()
@@ -14,3 +14,9 @@ carMakers.map((car: string): string => {
     return result
 })
 
+// This arr can receive a Date or string as value, either
+const importantDates: (Date | string)[] = [new Date(), '2022-10-10']
+importantDates.push('2021-01-01')
+importantDates.push(new Date())
+
+console.log(importantDates)
