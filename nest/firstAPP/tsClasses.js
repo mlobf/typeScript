a5cde9c2944b4f5f32587b66b62a01b77e0e0aa0
mlobf <marcoslemeborbafilho@gmail.com>
2022-07-08 00:18:56 -0300

    Basic inheritance in Ts

--- nest/firstAPP/tsClasses.js
+++ nest/firstAPP/tsClasses.js
@@ -1,1 +1,11 @@
-console.log('kjsdkjkdjfljsdlfj');
+// Classes in TS
+var Vehicle = /** @class */ (function () {
+    function Vehicle() {
+    }
+    Vehicle.prototype.drive = function () {
+        console.log('skjsfajfl');
+    };
+    return Vehicle;
+}());
+var car = new Vehicle();
+car.drive();
--- nest/firstAPP/tsClasses.ts
+++ nest/firstAPP/tsClasses.ts
@@ -1,5 +1,36 @@
 // Classes in TS
 
+class Vehicle {
+    drive(): void {
+        console.log('skjsfajfl')
+
+    }
+}
+
+class Car extends Vehicle {
+
+    // overwrite method
+    drive(): void {
+        console.log('skjsfajfl')
+
+    }
+
+    pupu(): void {
+        console.log('skjsfajfl')
+
+    }
+
+}
+
+
+const carro = new Car()
+
+
+
+
+
+
+
 
 
 
