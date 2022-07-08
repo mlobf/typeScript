2c4ebdba6c35e04bab669818c98880a1b936b603
mlobf <marcoslemeborbafilho@gmail.com>
2022-07-08 00:43:50 -0300

    Usando classes de uma forma um pouco melhor

--- nest/firstAPP/tsClasses.js
+++ nest/firstAPP/tsClasses.js
@@ -1,11 +1,57 @@
 // Classes in TS
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Vehicle = /** @class */ (function () {
     function Vehicle() {
     }
     Vehicle.prototype.drive = function () {
-        console.log('skjsfajfl');
+        console.log('we are driving');
+    };
+    Vehicle.prototype.calling = function () {
+        this.jump();
+    };
+    // protected
+    // apenas esta classe ou filhos 
+    //  desta classe podem usar este metodo
+    Vehicle.prototype.shoot = function () {
+        console.log('we are shooting');
+    };
+    // Only Vehicle are allowed to  use drive method
+    Vehicle.prototype.jump = function () {
+        console.log('We are jumping');
     };
     return Vehicle;
 }());
-var car = new Vehicle();
-car.drive();
+var Car = /** @class */ (function (_super) {
+    __extends(Car, _super);
+    function Car() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    // modificadores
+    //  public private protected
+    // overwrite method
+    Car.prototype.driv = function () {
+        console.log('skjsfajfl');
+        this.shoot();
+    };
+    Car.prototype.pupu = function () {
+        console.log('skjsfajfl');
+    };
+    return Car;
+}(Vehicle));
+var veiculo = new Vehicle();
+var carro = new Car();
+carro.driv();
--- nest/firstAPP/tsClasses.ts
+++ nest/firstAPP/tsClasses.ts
@@ -1,39 +1,40 @@
 // Classes in TS
 
 class Vehicle {
-    drive(): void {
-        console.log('skjsfajfl')
+    public drive(): void {
+        console.log('we are driving')
+    }
+    public calling(): void {
+        this.jump()
+    }
 
+    // protected
+    // apenas esta classe ou filhos 
+    //  desta classe podem usar este metodo
+    protected shoot(): void {
+        console.log('we are shooting')
+    }
+    // Only Vehicle are allowed to  use drive method
+    private jump(): void {
+        console.log('We are jumping')
     }
 }
 
 class Car extends Vehicle {
+    // modificadores
+    //  public private protected
 
     // overwrite method
-    drive(): void {
+    driv(): void {
         console.log('skjsfajfl')
-
+        this.shoot()
     }
-
-    pupu(): void {
+    private pupu(): void {
         console.log('skjsfajfl')
-
     }
-
 }
 
-
+const veiculo = new Vehicle()
 const carro = new Car()
 
-
-
-
-
-
-
-
-
-
-
-
-
+carro.driv()
