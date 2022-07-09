bd7f71f8c79fee9d937ed29a66fbcbe3efe6e147
mlobf <marcoslemeborbafilho@gmail.com>
2022-07-08 22:51:52 -0300

    Basic Setup For First Project in TS using Parcel

---
+++ nest/49-fields-inheritance/features/arrays.js
@@ -0,0 +1,16 @@
+var carMakers = ['ford', 'toyota', 'chevy'];
+var dates = [new Date(), new Date()];
+var carsByMake = [];
+// Help with inference when extracting values
+var car = carMakers[0];
+var myCar = carMakers.pop();
+// Prevent incompatible values
+carMakers.push(100);
+// Help with 'map'
+carMakers.map(function (car) {
+    return car.toUpperCase();
+});
+// Flexible types
+var importantDates = [];
+importantDates.push('2030-10-10');
+importantDates.push(new Date());
