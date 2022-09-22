e62e9203d2e75a448bb50a2d4cdf90af66d09fe3
mlobf <marcoslemeborbafilho@gmail.com>
2022-09-21 22:09:25 -0300

    simple update

--- justIntoToTypeScript/add.js
+++ justIntoToTypeScript/add.js
@@ -2,7 +2,13 @@ function add(n1, n2) {
     var result = n1 + n2;
     return result;
 }
+var oi = "oi";
 console.log(add(3, 5));
+var myArray = [1, 2, 3, 4, 5, 3, 6];
+for (var index = 0; index < myArray.length; index++) {
+    var element = myArray[index];
+    console.log(element);
+}
 var number1 = 5;
 var number2 = 5;
 var done = true;
--- justIntoToTypeScript/add.ts
+++ justIntoToTypeScript/add.ts
@@ -1,10 +1,18 @@
 function add(n1: number, n2: number) {
-	const result = n1 + n2;
-	return result;
+  const result = n1 + n2;
+  return result;
 }
 
-const ='oi'
+const oi = "oi";
+
 console.log(add(3, 5));
+
+const myArray = [1, 2, 3, 4, 5, 3, 6];
+
+for (let index = 0; index < myArray.length; index++) {
+  const element = myArray[index];
+  console.log(element);
+}
 
 const number1: number = 5;
 const number2: number = 5;
